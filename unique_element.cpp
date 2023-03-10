d550b558090685a94040b88d068fb13d7fcadefd
NeoT-800 <116675389+NeoT-800@users.noreply.github.com>
2023-03-11 00:01:00 +0530

    Update unique_element.cpp

--- unique_element.cpp
+++ unique_element.cpp
@@ -5,12 +5,36 @@ using namespace std;
 int findUnique(int *arr, int n) {
     // Write your code here
 
+    // Apptroach3: O(N)
+        int ans=0;
+    for(int i=0; i<n ;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+    
+    // Approach2: O(NlogN)
          sort(arr,arr+n);
     for (int i = 0; i < n; i+=2) {
         if (arr[i] != arr[i+1]) {
             return arr[i];
         }
     }
+    
+    // Approach3: O(N^2)
+            for(int i=0; i<n; i++){
+        bool isUnique=true;
+        for(int j=0; j<n; j++){
+          if (arr[i] == arr[j] && i != j) {
+            isUnique=false;
+            break;
+          }
+        }
+        if(isUnique){
+            return arr[i];
+        }
+    }
+    
+    
 }
 int main(){
     int n;
